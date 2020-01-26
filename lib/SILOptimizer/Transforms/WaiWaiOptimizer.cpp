0dbaea7f67beb1e91da795a0c2a037c02ea1c6f0
Nonchalant <afrontier829@gmail.com>
2020-01-26 14:52:34 +0900

    minDCE

--- lib/SILOptimizer/Transforms/WaiWaiOptimizer.cpp
+++ lib/SILOptimizer/Transforms/WaiWaiOptimizer.cpp
@@ -14,17 +14,30 @@ namespace {
 class WaiWaiOptimizer : public swift::SILFunctionTransform {
   /// The entry point to the transformation.
 private:
+
+  SmallSetVector<SILInstruction*, 32> Instructions;
     
   // [No.2]: minDCEを書く（必要であれば実装します） 
   // やること: 後片付け。次のFunctionのminDCEを回すときに残ってまずいものは、ここでどうにかしておくこと
   // 配列とか・・・？
   void onFinished() {
+    Instructions = SmallSetVector<SILInstruction*, 32>();
   }
 
   // [No.2]: minDCEを書く（関数を実装してください）      
   // やること: * UsefulなInsturctionの条件１を判定してMarkする　
   //          - BBの中からReturn Instを探して、それをUsefulとしてMarkする
   void checkReturnInstAndSetToLiveInst() {
+    for(auto& bb: *getFunction()) {
+      for (auto i = bb.begin(), E = bb.end(); i != E;) {
+        auto *inst = &*i;
+        i++;
+
+        if(isa<ReturnInst>(inst)) {
+          Instructions.insert(inst);
+        }
+      }
+    }
   }
     
   // [No.2]: minDCEを書く（関数を実装してください） 
@@ -34,15 +47,49 @@ private:
   // Hint1: すでにMarkしたものも十分に条件２で走査しましょう。再帰をつかうとすっきりします。
   // Hint2: 十分に走査した後は、MarkされたInstructionは増えません。つまり、Markされたものが増えたら走査するべき対象がまだあるということです。
   void propagateUseful() {
+    for (auto i = 0; i < int(Instructions.size()); i++) {
+      auto inst = Instructions[i];
+
+      for (auto& o: inst->getAllOperands()) {
+        if (auto i = o.get()->getDefiningInstruction()) {
+          if (!Instructions.count(i)) {
+            Instructions.insert(i);
+          }
+        }
+      }
+
+      if (auto valueI = dyn_cast<SingleValueInstruction>(inst)) {
+         for (auto useO: valueI->getUses()) {
+           if (auto useI = useO->getUser()) {
+             if (!Instructions.count(useI)) {
+               Instructions.insert(useI);
+             }
+           }
+         }
+       }
+    }
   }
     
   // [No.2]: minDCEを書く        
   // やること: Functionの中のUsefulでMarkされて *ない* Instructionをすべて削除する
   void deleteNotUseful() {
+    for(auto& bb: *getFunction()) {
+      for (auto i = bb.begin(), E = bb.end(); i != E;) {
+        auto *inst = &*i;
+        i++;
+
+        if (!Instructions.count(inst)) {
+          inst->replaceAllUsesOfAllResultsWithUndef();
+          inst->eraseFromParent();
+        }
+      }
+    }
   }
     
 public:
   void run() override {
+    if (!getOptions().WaiWaiOption)
+    return;
       
     // BBが2つ以上ある or terminatorがreturnじゃないFunctionは無視
     // 資料にある問題２の実装です。
@@ -57,10 +104,10 @@ public:
             
     // [No.2]: minDCEを書く（実装）       
     // Hint: ここでは、propagateUsefulとcheckReturnInstAndSetToLiveInstとdeleteNotUsefulを適切な順番で呼ぶこと
+    checkReturnInstAndSetToLiveInst();
+    propagateUseful();
+    deleteNotUseful();
 
-
-
-        
     // 後片付け、書き換えないで良い
     onFinished();
   }
